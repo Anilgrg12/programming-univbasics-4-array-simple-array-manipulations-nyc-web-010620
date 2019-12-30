196c9ab847973d8703c9e5c79e02a6797a23bfe9
amusing-transpiler-2573 <anil_grg@hotmail.com>
2019-12-30 09:39:31 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,4 +2,5 @@ def using_push(colors_in_the_rainbow,next_color)
   updated_color= colors_in_the_rainbow.push("violet")
 end
 
-def using_unshift(bouroughs_in_nyc,)
+def using_unshift(bouroughs_in_nyc,new_neighborhood)
+  updated_array= bouroughs_in_nyc.unshift("")
